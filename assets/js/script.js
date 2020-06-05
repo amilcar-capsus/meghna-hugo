119bfaca2fbe746f716eae1d7908af145c8f93af
José Amílcar González Miranda <joseamilcargonzalezmiranda@iMac-de-Juan.local>
2020-06-05 13:43:04 -0500

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,7 +155,7 @@ jQuery(function ($) {
 		// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
 		// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 		var modal = $(this)
-		modal.find('.modal-title').text(name)
+		modal.find('.modal-title').text(decodeURIComponent(name))
 		modal.find('.modal-body #member-picture').attr('alt',name).attr('src', picture)
 		modal.find('.modal-body #member-profile').text(profile)
 	  })
