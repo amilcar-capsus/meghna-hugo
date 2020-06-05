a94fa38233bdc0adfc967ad929786dd22d70c6ce
José Amílcar González Miranda <joseamilcargonzalezmiranda@iMac-de-Juan.local>
2020-06-05 13:49:28 -0500

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -150,6 +150,8 @@ jQuery(function ($) {
 	$('#modalTeam').on('show.bs.modal', function (event) {
 		var button = $(event.relatedTarget) // Button that triggered the modal
 		var name = button.data('name') // Extract info from data-* attributes
+		console.log('name')
+		console.log(decodeURIComponent(name))
 		var picture = button.data('image') // Extract info from data-* attributes
 		var profile = button.data('profile') // Extract info from data-* attributes
 		// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
