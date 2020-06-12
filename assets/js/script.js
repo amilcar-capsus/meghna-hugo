f9415f36eaba8e677660750972b9058a05cd1337
José Amílcar González Miranda <joseamilcargonzalezmiranda@iMac-de-Juan.local>
2020-06-12 14:58:24 -0500

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,7 +190,7 @@ jQuery(function ($) {
 		function moveTop(){
 		  $('.scroll-titles ul').animate({
 			top : -tickerHeight
-		  },300, function(){
+		  },1000, function(){
 		   $('.scroll-titles ul li:first-child').appendTo('.scroll-titles ul');
 			$('.scroll-titles ul').css('top','');
 		  });
